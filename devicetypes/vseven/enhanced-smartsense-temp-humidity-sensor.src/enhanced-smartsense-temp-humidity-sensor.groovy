ce6e5de92d5b8b8033e46fe6d9c134fc573fa2c2
Allan <vseven@yahoo.com>
2016-07-24 16:15:09 -0400

    Update enhanced-smartsense-temp-humidity-sensor.groovy

--- devicetypes/vseven/enhanced-smartsense-temp-humidity-sensor.src/enhanced-smartsense-temp-humidity-sensor.groovy
+++ devicetypes/vseven/enhanced-smartsense-temp-humidity-sensor.src/enhanced-smartsense-temp-humidity-sensor.groovy
@@ -46,7 +46,7 @@ metadata {
 		}
 
 	tiles(scale: 2) {
-		multiAttributeTile(name:"temperature", type: "generic", width: 6, height: 4,canChangeIcon: yes){
+		multiAttributeTile(name:"temperature", type: "generic", width: 6, height: 4,canChangeIcon: true){
 			tileAttribute ("device.temperature", key: "PRIMARY_CONTROL") {
 				attributeState "temperature", label:'${currentValue}°',
 					backgroundColors:[
