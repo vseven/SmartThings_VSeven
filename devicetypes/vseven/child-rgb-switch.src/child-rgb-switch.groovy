af48dc3786503b15b1d5f1bcd3930ede61d1af6f
Allan <vseven@yahoo.com>
2017-10-06 11:44:38 -0400

    Update child-rgb-switch.groovy
    Added dimming.  Not fully finished

--- devicetypes/vseven/child-rgb-switch.src/child-rgb-switch.groovy
+++ devicetypes/vseven/child-rgb-switch.src/child-rgb-switch.groovy
@@ -63,11 +63,11 @@ void on() {
 	log.debug("On pressed.  Turning status on and sending last known HEX value of $lastColor")
 	//parent.childOn(device.deviceNetworkId)
 	// Send the last hex value to turn back on
-	//parent.childSetColor(device.deviceNetworkId, device.latestValue("color"))
+	//parent.childSetColor(device.deviceNetworkId, lastColor)
 }
 
 void off() {
-	log.debug("Off pressed.  Sending HEX of #000000")
+	log.debug("Off pressed.  Sending HEX of #000000 but not updating device (retain last set color).")
 	//parent.childOff(device.deviceNetworkId)
 	// Send a all 0 hex value to turn off the LED
 	//parent.childSetColor(device.deviceNetworkId, "#000000")
@@ -77,18 +77,28 @@ def setColor(value) {
     log.debug("Color value in hex: $value.hex")
     // If the color is being changed we should also turn on
     //parent.childOn(device.deviceNetworkId)
-    //parent.childSetColorRGB(device.deviceNetworkId, value.hex)
+    // If selecting a color and device is off then turn it on
+
 }
 
 def setLevel(value) {
     log.debug("Level value in percentage: $value")
+    sendEvent(name: "level", value: level, unit: "%")
+    //parent.childSetLevel(device.deviceNetworkId, level)
+	
     // Turn on or off based on level selection
-    if (level == 0) { off() }
+    if (level == 0) { off() } 
     else if (device.latestValue("switch") == "off") { on() }
-    // Get the last known color and if null use full on
-    def colorHex = device.latestValue("color")
-    if (colorHex == null)
-	colorHex = "#FFFFFF"
+    // Then if level is above 0 adjust the color
+    if (level > 0) {
+	// Get the last known color and if null use full on
+	def colorHex = device.latestValue("color")
+	if (colorHex == null) {colorHex = "#FFFFFF"}
+	adjustColor(colorHex,level)
+    }
+}
+
+def adjustColor(hex, level) {
     // Convert the hex color, apply the level, then send to the setColor routine
     def c = hexToRgb(colorHex)
 
@@ -99,9 +109,17 @@ def setLevel(value) {
     def adjustedColor = "#" + $r + $g + $b
     log.debug("Adjusted color is $adjustedColor")
 	
-    sendEvent(name: "level", value: level, unit: "%")
-    //parent.childSetLevel(device.deviceNetworkId, level)
-    setColor(adjustedColor)
+    //parent.childSetColorRGB(device.deviceNetworkId, adjustedColor)
+}
+
+def hexToRgb(colorHex) {
+    def rrInt = Integer.parseInt(colorHex.substring(1,3),16)
+    def ggInt = Integer.parseInt(colorHex.substring(3,5),16)
+    def bbInt = Integer.parseInt(colorHex.substring(5,7),16)
+
+    def colorData = [:]
+    colorData = [r: rrInt, g: ggInt, b: bbInt]
+    colorData
 }
 
 def generateEvent(String name, String value) {
